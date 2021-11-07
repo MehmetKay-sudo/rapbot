e411b0f60c37725e83899e57a9b800db7307d7d7
MehmetKay-sudo <mehmet.kay.programming@gmail.com>
2021-11-07 11:08:52 +0100

    I push now the first code example for the bot with syntax01.

--- rapbot_testing/rapbot_testing.py
+++ rapbot_testing/rapbot_testing.py
@@ -2,6 +2,7 @@ import random
 
 # battle starts
 print("You wanna rhyme against me? " + "Let's see, what you got!")
+print("Are you ready?")
 
 # building syntax
 pronouns = (str("he"), str("she"), str("it"))
@@ -16,4 +17,16 @@ for i in syntax01:
         print(pronouns[0:1])
         print(verbs[0:1])
         print(adjectives[0:1])
+    break
 
+# separate the rhyme
+print(", ")
+
+# creating second syntax
+pronouns2 = (str("he"), str("she"), str("it"), str("they"))
+verbs2 =
+
+# aim for the next session
+# create an input function call, through which the player can interact with the bot
+# 1st option: player presses one word into the call and bot gives a word back
+# 2nd option: bot rhymes one rhyme and the player has to complete the rhyme line
