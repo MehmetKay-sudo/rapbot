aa2618455407ca677d3ffa2cea744909ac7f4b14
MehmetKay-sudo <mehmet.kay.programming@gmail.com>
2021-11-07 11:03:02 +0100

    I push now the first code example for the bot with syntax01.

--- rapbot_testing/rapbot_testing.py
+++ rapbot_testing/rapbot_testing.py
@@ -1,7 +1,7 @@
 import random
 
 # battle starts
-print("You wanna rhyme against me?" + "Let's see, what you got!")
+print("You wanna rhyme against me? " + "Let's see, what you got!")
 
 # building syntax
 pronouns = (str("he"), str("she"), str("it"))
@@ -13,6 +13,7 @@ syntax01 = [pronouns, verbs, adjectives]
 # creating loops for every syntax-complex
 for i in syntax01:
     if i in syntax01:
-        print("This is the 1st round!")
-
+        print(pronouns[0:1])
+        print(verbs[0:1])
+        print(adjectives[0:1])
 
