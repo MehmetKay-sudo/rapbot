d8d023729efc4bd9d6b88fc615aee742a7614829
MehmetKay-sudo <mehmet.kay.programming@gmail.com>
2021-11-25 22:00:27 +0100

    This is an updated version of the rapbot with time-function.

--- rapbot_testing/rapbot_testing.py
+++ rapbot_testing/rapbot_testing.py
@@ -1,6 +1,6 @@
 import time
 import os
-
+# START // INITIALISATION-----------------------------------------------------------------------------------------------
 # establish list of names
 names = [str("cherry"), str("billy"), str("rose")]
 
@@ -43,41 +43,87 @@ print("Let's go in..." + "1" + "...")
 # clear counting in program
 # os.system("clear")
 
+# RAP BATTLE // ACTUAL ENCOUNTER----------------------------------------------------------------------------------------
+# 1ST ROUND OF BATTLE
 # building syntax
 time.sleep(1)
 pronouns = (str("he"), str("she"), str("it"))
 verbs = (str("does"), str("rhymes"), str("lies"))
 adjectives = (str("big"), str("small"), str("tough"))
 
-syntax01 = [pronouns, verbs, adjectives]
+# mix 1st round syntax
+syntax_bot = [pronouns, verbs, adjectives]
 
 # creating loops for Syntax01
-for i in syntax01:
-    if i in syntax01:
+for i in syntax_bot:
+    if i in syntax_bot:
         print(pronouns[0:1])
         print(verbs[0:1])
         print(adjectives[0:1])
     break
 
-# separate the rhyme
-print(", ")
+# player has to put input into the game
+rhyme_user = str(input("Enter your rhyme sucker: "))
 
+# compare rhyme from user with syntax from bot
+for i in syntax_bot:
+    if rhyme_user == "he is small":
+        print("Nice try fella! ")
+        time.sleep(1)
+        print("Try again! ")
+    print(rhyme_user)
+
+    if rhyme_user == "he does big":
+        print("Why are you telling the same thing, as i do? ")
+        time.sleep(1)
+        print("Try again! ")
+    print(rhyme_user)
+
+    if rhyme_user == "he sucks hard! ":
+        print("You beat me once" + ", " + "Let's see, what you will do in the next round! ")
+        time.sleep(15)
+    break
+
+# 2ND ROUND OF BATTLE---------------------------------------------------------------------------------------------------
 # creating second syntax
 pronouns2 = (str("he"), str("she"), str("it"), str("they"))
 verbs2 = (str("think"), str("drink"), str("blink"))
 adjectives2 = (str("great"), str("greedy"), str("fast"))
 
-syntax02 = [pronouns2, verbs2, adjectives2]
+syntax_bot2 = [pronouns2, verbs2, adjectives2]
 
 # creating loop2 for syntax02
-for i in syntax02:
-    if i in syntax02:
+for i in syntax_bot2:
+    if i in syntax_bot2:
         print(pronouns2[0:1])
         print(verbs2[0:1])
         print(adjectives2[0:1])
     break
 
-print(", ")
+# player has to put input into the game
+rhyme_user = str(input("Enter your rhyme sucker: "))
+
+# compare rhyme from user with syntax from bot
+for i in syntax_bot2:
+    if rhyme_user == "he is dumb ":
+        print("Nice try fella! ")
+        time.sleep(1)
+        print("Try again! ")
+    print(rhyme_user)
+
+    if rhyme_user == "he thinks great ":
+        print("Why are you telling the same thing, as i do? ")
+        time.sleep(1)
+        print("Try again! ")
+    print(rhyme_user)
+
+    if rhyme_user == "he tries to be smart! ":
+        print("You beat me once, let us see next round")
+    break
+
+
+
+
 
 # this is the code for 2 bars.
 # aim for the next session
