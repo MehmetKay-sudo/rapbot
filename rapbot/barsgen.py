f44fc8020a783ee4840b2bdda9a8fed5e240e777
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-06-28 18:15:13 +0200

    Update barsgen.py

--- rapbot/barsgen.py
+++ rapbot/barsgen.py
@@ -1,11 +1,22 @@
 # create two lists with letters as strings
 letters_big = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 letters_small = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+nouns = ["Fast", "Fear", "Feeders", "Female", "Florida", "Fish", "Wildlife", "Conservation", "Commission", "Food", "Freshwater"]
+verbs = ["apply", "ride", "create", "drive", "sing", "feel", "brake"]
+adjectives = ["big". "small", "long", "short", "nice", "quite", "loud"]
+
+# idea of a context linker is, to link words, which makes sense in a context
+def context_linker():
+  nouns[5]
+  verbs[5]
+  adjectives[1]
+  context1 = (nouns[5], verbs[5], adjectives[1])
+  print(context1)
 
 # create a bowl of letters
 letters_general = [letters_big, letters_small]
 
-count = 0
+#count = 0
 
 #def word():
   
