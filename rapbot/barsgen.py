31c26493bd11a1dbd398a6a3388b975a1695ef7b
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-06-28 17:35:27 +0200

    Create barsgen.py

---
+++ rapbot/barsgen.py
@@ -0,0 +1,16 @@
+# create two lists with letters as strings
+letters_big = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+letters_small = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+# create a bowl of letters
+letters_general = [letters_big, letters_small]
+
+count = 0
+
+#def word():
+  
+
+# create a for loop
+for i in letters_general:
+  if i in letters_general:
+    #print(word)
