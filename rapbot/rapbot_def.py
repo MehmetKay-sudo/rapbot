c13b8cd93913000b61eed615ce64e9bb33f4287a
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-08-01 00:49:22 +0200

    few changes in functions of generating bars, needs to be tested

--- rapbot/rapbot_def.py
+++ rapbot/rapbot_def.py
@@ -1,11 +1,31 @@
+
+# write a main function
+def main():
+    print(bars)
+    print(bars2)
+    print(bars3)
+
 # create a bar_generator / specific
 def bars():
-  bar1 = str("I rhyme fine!")
-  bar2 = str("I rhyme great!")
-  bar3 = str("I am the best in the state!")
-
-  bar_bowl = [bar1, bar2, bar3]
-  return bar_bowl
+    bar1 = str("I rhyme fine!")
+    bar2 = str("I rhyme great!")
+    bar3 = str("I am the best in the state!")
 
 bars = bars()
-print(bars, end="")
+
+def bars2():
+    bar4 = str("I am straight! ")
+    bar5 = str("Big my brain! ")
+    bar6 = str("Skilled and insane! ")
+
+bars2 = bars()
+
+def bars3():
+    bar7 = str("Killer instinct. ")
+    bar8 = str("Kill you with one ink. ")
+    bar9 = str("Don't try me, otherwise you go exstinct. ")
+
+bars3 = bars3()
+
+bars = (bars, bars2, bars3)
+print(bars)
