a27f2e64b55434575af6ca3f87e146b4c3b4bdec
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-08-01 22:52:48 +0200

    tried to write an bars generator and wrapped them into functions

--- rapbot/def_start.py
+++ rapbot/def_start.py
@@ -30,21 +30,3 @@ bars3 = bars3()
 
 bars = (bars, bars2, bars3)
 print(bars)
-
-# create an input() to start processing bars
-# impulse = input("Press 'A', 'B', 'C'" + "and start the rap battle: ")
-
-# create a for loop and get rhymes
-def dummy():
-    import time
-    print("You will get rhymes and have the choice to write a rhyme back. ")
-    time.sleep(1)
-    print("You have the options 'rhyme1', 'rhyme2' and 'rhyme3'. ")
-    time.sleep(1)
-    print(". " + ". " + ". ")
-    dummy = input("Press 'rhyme1' " + "to get the first line " + "and start the rap battle: ")
-
-# choices and buttons
-rhyme1 == "Get me some"
-rhyme2 == "You get done"
-rhyme3 == "To battle makes fun"
