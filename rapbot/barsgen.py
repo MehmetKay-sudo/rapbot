baec915649f576c2b4c388d700617a266fc49fb5
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-08-01 23:05:42 +0200

    tried to package letters and words into functions. Aim here is to write an generator, which can generate syntax and words - like a language module.

--- rapbot/barsgen.py
+++ rapbot/barsgen.py
@@ -1,3 +1,6 @@
+def main():
+
+
 # create two lists with letters as strings
 letters_big = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 letters_small = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -7,22 +10,28 @@ adjectives = ["big", "small", "long", "short", "nice", "quite", "loud"]
 
 # idea of a context linker is, to link words, which makes sense in a context
 def context_linker():
-  nouns[5]
-  verbs[5]
-  adjectives[1]
-  context1 = (nouns[5], verbs[5], adjectives[1])
-  print(context1)
+    context = [letters_big, letters_small]
+    link = input("Enter link to generate a word: ")
+    if link == "link"
+    print(link)
 
-# create a bowl of letters
-letters_general = [letters_big, letters_small]
-
-# create a bowl of words
-words = [nouns, verbs, adjectives]
-
-def word_set():
-    print(letters_general)
+#a function to link words into sentences
+def context_linker2():
+    words = [nouns, verbs, adjectives]
+    link2 = input("Enter link2 to generate a syntax: ")
+    if link2 == "link2"
     print(words)
 
-words = word_set()
+#create a bowl of letters
+#letters_general = [letters_big, letters_small]
 
-print(words)
+#create a bowl of words
+#words = [nouns, verbs, adjectives]
+
+#def word_set():
+#    print(letters_general)
+#    print(words)
+
+#words = word_set()
+
+#print(words)
