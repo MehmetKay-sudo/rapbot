47255252070e7adae841cec23074fd00ac44a55a
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-08-06 20:30:06 +0200

    tested functions, just to know, how the correlate with each other

--- rapbot_testing/message.py
+++ rapbot_testing/message.py
@@ -1,5 +1,35 @@
-nouns = str("Bad", "Strand", "Süßigkeiten")
+import time
+import timer
+import random as rn
 
-verbs = str("gehen", "laufen", "drehen")
+def main():
+    print(syntax)
+    print(battle)
 
-adjectives = str("blau", "rund", "alt")
+def sleep2():
+    time.sleep(2)
+sleep = sleep2()
+
+def syntax():
+    nouns = "bad", "beach", "sweets"
+    verbs = "go", "walk", "turn"
+    adjectives = "blue", "round", "old"
+    syntax = [(nouns, verbs, adjectives)]
+
+syntax = syntax()
+
+def battle():
+    rhyme = "I rhyme hard"
+    rhyme2 = "You lost sight"
+    rhyme3 = "We might fight"
+    rhymes = [rhyme, rhyme2, rhyme3]
+    print(sleep)
+    rhyme = input("Battle back: ")
+
+battle = battle()
+
+def print2():
+    print(syntax)
+    print(battle)
+
+print(print2)
