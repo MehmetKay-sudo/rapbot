c8ccb6525359b22c1b61c2c376d8bdfb94961930
MehmetKay-sudo <mehmet.kay.programming@gmail.com>
2022-08-07 19:04:21 +0200

    deleted some files and edited a program called rhymegen. Aim is to write a module to make the bot rap - a generator that can be used for several rounds.

--- rapbot_testing/syntax.py
+++ rapbot_testing/syntax.py
@@ -17,19 +17,3 @@ def syntax():
     syntax = [(nouns, verbs, adjectives)]
 
 syntax = syntax()
-
-def battle():
-    rhyme = "I rhyme hard"
-    rhyme2 = "You lost sight"
-    rhyme3 = "We might fight"
-    rhymes = [rhyme, rhyme2, rhyme3]
-    print(sleep)
-    rhyme = input("Battle back: ")
-
-battle = battle()
-
-def print2():
-    print(syntax)
-    print(battle)
-
-print(print2)
