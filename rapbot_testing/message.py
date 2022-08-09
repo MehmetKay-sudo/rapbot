d54df025e7ef8e601f4434daa4304186600fee24
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-08-09 23:25:54 +0200

    removed some files

---
+++ rapbot_testing/message.py
@@ -0,0 +1,35 @@
+import time
+import timer
+import random as rn
+
+def main():
+    print(syntax)
+    print(battle)
+
+def sleep2():
+    time.sleep(2)
+sleep = sleep2()
+
+def syntax():
+    nouns = "bad", "beach", "sweets"
+    verbs = "go", "walk", "turn"
+    adjectives = "blue", "round", "old"
+    syntax = [(nouns, verbs, adjectives)]
+
+syntax = syntax()
+
+def battle():
+    rhyme = "I rhyme hard"
+    rhyme2 = "You lost sight"
+    rhyme3 = "We might fight"
+    rhymes = [rhyme, rhyme2, rhyme3]
+    print(sleep)
+    rhyme = input("Battle back: ")
+
+battle = battle()
+
+def print2():
+    print(syntax)
+    print(battle)
+
+print(print2)
