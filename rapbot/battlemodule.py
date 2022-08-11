610602e1800e33552dfccfe21ae5b6a3eef1c483
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-08-11 17:02:40 +0200

    adjusted battlemodule.py, still not useful to this stage.

--- rapbot/battlemodule.py
+++ rapbot/battlemodule.py
@@ -10,26 +10,35 @@ import random as rn
 
 # introduction
 def bars(x, y):
-    return x+y
     import time
     x = "I am the rapbot. " + "call me the rapgod. "
     time.sleep(1)
     y = "Try to beat me. " + "This will not be easy. "
+    print(x)
+    print(y)
+    bars = bars(x,y)
+print(bars)
 
 # print rhyme choices
 def counter_rhymes(g, h):
-    return g+h
     g = "1" + ": " + "You are here to sing. " + "Not here to win. " + "You are kid with no wing. "
     h = "2" + ": " + "This rap battle is going to be heated! " + "An ambulance will be needed. "
+    print(g)
+    print(h)
+    counter_rhymes = counter_rhymes(g,h)
+print(counter_rhymes)
 
 # generate input function
 counter = input("Choose rhyme for counter attack through pressing 1 and 2: ")
 
 # write generate for measuring options
-def rhymegen():
-    while True:
-        for counter in counter_rhymes:
-            if counter == counter_rhyme1:
-                print("You beat me. " + "let's go to the next round. ")
-            else:
-                print("This is not enough sucker! " + "Try again. ")
+while True:
+    if counter == counter_rhymes:
+        print("You beat me. " + "let's go to the next round. ")
+    else:
+        print("This is not enough sucker! " + "Try again. ")
+        break
+
+# functions output:
+# <function bars at 0x7f3d0f2c90d0>
+# <function counter_rhymes at 0x7f3d0f04bdc0>
