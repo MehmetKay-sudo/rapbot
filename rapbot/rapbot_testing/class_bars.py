c62b061fa561c6463e179e79c21a40e1f8163781
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-08-23 14:16:22 +0200

    Update class_bars.py
    not working right now

--- rapbot/rapbot_testing/class_bars.py
+++ rapbot/rapbot_testing/class_bars.py
@@ -1,6 +1,14 @@
+# first you definde the function
 def class(a,b):
   def bar(a):
     a = "Let's rhyme and dance" + "/"
+    print(a)
   def bar(b):
     b = "Try to diss me, you have no chance" + "/"
-print(a,b)
+   print(b)
+  
+# you tell what to do with the function
+input_a = input("Press a")
+print(a)
+input_b = input("Press b)
+print(b)
