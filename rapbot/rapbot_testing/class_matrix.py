341531401fd20408f346d219664d58ebc8f31fe4
Mehmet Kücük <mehmet.kay.programming@gmail.com>
2023-01-31 01:18:17 +0100

    deleted testing folder

--- rapbot/rapbot_testing/class_matrix.py
+++
@@ -1,7 +0,0 @@
-class bar_matrix():
-  def bar(x,y):
-    x = "The star is shining bright"
-    y = "I'll be allright"
-   def bar(a,b):
-    a = "Let's rap on sight"
-    b = "All day and all night"
