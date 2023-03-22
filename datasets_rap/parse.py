999a0f7d8ad94513e9d3a77e09a6768787dd662f
Mehmet Kücük <mehmet.kay.programming@gmail.com>
2023-03-22 17:39:15 +0100

    checking with two tokens

--- datasets_rap/parse.py
+++ datasets_rap/parse.py
@@ -1,8 +1,16 @@
+# import the nltk library
 import nltk
 
+# import a specific module word_tokenize
 from nltk import word_tokenize
 
-raptext = open(""datasets_rap/eminem.txt""), "r").read()
+# define a variable which raptext
+# open the raptext and read it
+# checking two different tokens 
+raptext = open("datasets_rap/eminem.txt", "r").read()
 tokens = word_tokenize(raptext)
 print(tokens)
 
+raptext2 = open("datasets_rap/ceza.txt", "r").read()
+tokens2 = word_tokenize(raptext)
+print(tokens2)
